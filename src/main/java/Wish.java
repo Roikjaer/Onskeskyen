972969575f8ec19fb1837499e564ba1e767eedc0
Nicolai_Granslev <Granslev.dk@gmail.com>
2026-04-09 14:18:27 +0200

    t1

---
+++ src/main/java/Wish.java
@@ -0,0 +1,22 @@
+class Wish {
+    private String name;
+    private String description;
+
+    public Wish(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + description;
+    }
+}
