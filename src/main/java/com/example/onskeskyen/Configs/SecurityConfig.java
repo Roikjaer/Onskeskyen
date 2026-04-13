7c41294b1a3e369facb818289b4fdbc9f1cb1fd5
Nicolai_Granslev <Granslev.dk@gmail.com>
2026-04-13 11:36:15 +0200

    Kæmpe_Update4

--- src/main/java/com/example/onskeskyen/Configs/SecurityConfig.java
+++ src/main/java/com/example/onskeskyen/Configs/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .userDetailsService(detailsService)   // ⭐ VIGTIGT FIX
+                .userDetailsService(detailsService)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/register").permitAll()
                         .anyRequest().authenticated()
